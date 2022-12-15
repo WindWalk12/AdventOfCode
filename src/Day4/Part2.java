4c78877430e332645b5cd18e50e5de8b3edd81ee
WindWalk <nhygta@hotmail.com>
2022-12-15 08:33:16 +0100

    Added part2, not solved yet.

--- src/Day4/Part2.java
+++ src/Day4/Part2.java
@@ -1,4 +1,29 @@
 package Day4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/Day4/input.txt");
+        Scanner sc = new Scanner(file);
+        int result = 0;
+
+        while (sc.hasNext()) {
+            String[] chars1 = sc.nextLine().split(",");
+
+            int number1 = Integer.parseInt(chars1[0].split("-")[0]);
+            int number2 = Integer.parseInt(chars1[0].split("-")[1]);
+            int number3 = Integer.parseInt(chars1[1].split("-")[0]);
+            int number4 = Integer.parseInt(chars1[1].split("-")[1]);
+
+            if (number4 >= number2 && number1 >= number3) {
+                result++;
+            } else if (number2 >= number4 && number3 >= number1) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
 }
