50c2733eb2bf9a6e96fdddc99eb37a74937409c1
WindWalk <nhygta@hotmail.com>
2022-12-20 10:49:56 +0100

    Added Day5

---
+++ src/AoC22/Day5/Part1.java
@@ -0,0 +1,17 @@
+package AoC22.Day5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Part1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/AoC22/Day4/input.txt");
+        Scanner sc = new Scanner(file);
+        int result = 0;
+
+        while (sc.hasNext()) {
+        }
+        System.out.println(result);
+    }
+}
